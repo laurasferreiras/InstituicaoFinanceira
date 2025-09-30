bf05952219200de1f7019a7a3171aeb2b0170d7c
laurasferreiras <laurasferreira6@gmail.com>
2025-09-30 20:26:57 -0300

    conta com numero e saldo

---
+++ ControleContas/Conta.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ControleContas
+{
+    public class Conta
+    {
+        public Conta(long numero) 
+        {
+            this.Numero = numero;
+        }
+
+        public Conta() 
+        {
+            _numero = new Random().Next(10, 999999999);
+        }
+
+        private long _numero;
+        public long Numero
+        {
+            get
+            {
+                return _numero;
+            }
+            private set
+            {
+                _numero = value;
+            }
+        }
+
+
+        private decimal _saldo;
+        public decimal Saldo { get { return _saldo; } private set { _saldo = value; } }
+
+    }
+}
--- ControleContas/Program.cs
+++ ControleContas/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ControleContas;
+
+Conta conta1 = new Conta(123456789);
+Conta conta2 = new Conta();
+Console.WriteLine($"O número da conta é: {conta1.Numero}");
+Console.WriteLine($"O número da conta é: {conta2.Numero}");
+
+Console.WriteLine($"O saldo é de ");
+
+// crie um atributo saldo na classe conta cuja propriedade seja somente leitura 
